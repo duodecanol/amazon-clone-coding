b23c978ce94f9f6f6d8d3c8e26665ad24598fce3
HOME-2700 <metalhwal@gmail.com>
2021-06-17 00:49:06 +0900

    Header and Home

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img 
+          className="home__image"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/June/Fuji_TallHero_Gamers_en_US_1x._CB667161802_.jpg"
+          alt=""
+        />
+
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+        
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+        
+        <div className="home__row">
+          {/* Product */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/Product.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Product.css'
+
+function Product() {
+  return (
+    <div className="product">
+      {/* name */}
+      {/* price */}
+      {/* rating */}
+      {/* thumbnail */}
+      {/* add to basket */}
+    </div>
+  )
+}
+
+export default Product
