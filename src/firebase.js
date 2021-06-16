13e1f28e0271f409af5cfc31168d6422d632a60b
HOME-2700 <metalhwal@gmail.com>
2021-06-16 21:43:24 +0900

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBRZTi7xwrhF96qYVZnPOfyugnCmspFjEI",
+  authDomain: "clone-coding-5e9d8.firebaseapp.com",
+  projectId: "clone-coding-5e9d8",
+  storageBucket: "clone-coding-5e9d8.appspot.com",
+  messagingSenderId: "536893250923",
+  appId: "1:536893250923:web:ef4d739a37d09d807b802e"
+};
